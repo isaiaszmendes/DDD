aeab6b3918b8cb99e62c6c37dfa2ae2886bb869b
Isaias Mendes <isaiaszmendes@gmail.com>
2023-01-31 23:46:22 -0300

    feat: validate order

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -7,13 +7,27 @@ export type OrderProps = {
 }
 
 export class Order {
-  _id: string;
-  _customerId: string;
-  _items: Array<OrderItem>
+  private _id: string;
+  private _customerId: string;
+  private _items: Array<OrderItem>
+  private _total: number;
 
   constructor({ id, customerId, items }: OrderProps) {
     this._id = id;
     this._customerId = customerId;
     this._items = items;
+    this._total = this.total();
+    this.validate()
+  }
+
+  validate(): boolean {
+    if(this._id.length === 0) throw new Error("id is required")
+    if(this._customerId.length === 0) throw new Error("customerId is required")
+    if(this._items.length === 0) throw new Error("items length must be greater than 0")
+    return true
+  }
+
+  total() {
+    return this._items.reduce((acc, item) => acc + item._price, 0)
   }
 }
