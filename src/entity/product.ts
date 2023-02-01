a5dd16bf92d26a128116a2b3b1c2eb4a5c8469c5
Isaias Mendes <isaiaszmendes@gmail.com>
2023-02-01 01:04:43 -0300

    feat: add product

---
+++ src/entity/product.ts
@@ -0,0 +1,44 @@
+type ProductProps = {
+  id: string;
+  name: string;
+  price: number;
+}
+
+export class Product {
+  private _id: string;
+  private _name: string;
+  private _price: number;
+
+  constructor({ id, name, price }: ProductProps) {
+    this._id = id;
+    this._name = name;
+    this._price = price;
+    this.validate();
+  }
+
+  validate(): boolean {
+    // validate should receive params to validate
+    if(this._id.length === 0) throw new Error("id is required");
+    if(this._name.length === 0) throw new Error("name is required");
+    if(this._price < 0) throw new Error("price must be be greater than zero");
+    return true;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  changeName(name: string): void {
+    this._name = name;
+    this.validate()
+  }
+  
+  get price(): number {
+    return this._price;
+  }
+
+  changePrice(price: number): void {
+    this._price = price;
+    this.validate();
+  }
+}
