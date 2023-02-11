4d7eb8a30bd080570cc4fd51c769726ebedf3390
Isaias Mendes <isaiaszmendes@gmail.com>
2023-02-11 02:22:05 -0300

    feat: implements update order repository

--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -20,13 +20,41 @@ export class OrderRepository implements OrderRepositoryInterface {
 			include: [ { model: OrderItemModel } ]
 		});
 	}
-	update(entity: Order): Promise<void> {
-		throw new Error('Method not implemented.');
+	async update(entity: Order): Promise<void> {
+		const sequelize = OrderModel.sequelize;
+		await sequelize.transaction(async (t) => {
+			await OrderItemModel.destroy({
+				where: { order_id: entity.id },
+				transaction: t,
+			});
+			const items = entity.items.map((item) => ({
+				id: item.id,
+				name: item.name,
+				price: item.price,
+				product_id: item.productId,
+				quantity: item.quantity,
+				order_id: entity.id,
+			}));
+			await OrderItemModel.bulkCreate(items, { transaction: t });
+			await OrderModel.update(
+				{ customer_id: entity.customerId, total: entity.total() },
+				{ where: { id: entity.id }, transaction: t },
+			);
+		});
 	}
-	find(id: string): Promise<Order> {
-		throw new Error('Method not implemented.');
+
+	async find(id: string): Promise<Order> {
+		const order = await OrderModel.findOne({ where: { id }, rejectOnEmpty: true, });
+		console.log({ order: order.items });
+		const newOrder = new Order({
+			id: order.id,
+			customerId: order.customer_id,
+			items: []
+		});
+		return newOrder;
 	}
 	findAll(): Promise<Order[]> {
 		throw new Error('Method not implemented.');
 	}
+
 }
