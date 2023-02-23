8b4794cbab0ddec384082c93f3563fdcfe5e04a0
Isaias Mendes <isaiaszmendes@gmail.com>
2023-02-22 23:35:42 -0300

    feat: add event to notify when customer address is changed

---
+++ src/domain/event/customer/customer-changed-address.event.ts
@@ -0,0 +1,11 @@
+import { EventInterface } from '../@shared/event.interface';
+
+export class CustomerChangedAddressEvent implements EventInterface {
+	dateTimeOcurred: Date;
+	eventData: any;
+
+	constructor(eventData: any) {
+		this.eventData = eventData;
+		this.dateTimeOcurred = new Date();
+	}
+}
