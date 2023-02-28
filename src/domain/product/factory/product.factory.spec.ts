55fd4946a86520a383a8f9f392d81eca84b3e2c7
Isaias Mendes <isaiaszmendes@gmail.com>
2023-02-28 00:08:12 -0300

    test: create a productB using factory

--- src/domain/product/factory/product.factory.spec.ts
+++ src/domain/product/factory/product.factory.spec.ts
@@ -8,4 +8,13 @@ describe('Product factory unit test', () => {
 		expect(product.price).toBe(1);
 		expect(product.constructor.name).toBe('Product');
 	});
+
+	it('Should create a product type b', () => {
+		const product = ProductFactory.create({ type: 'b', name: 'Product B', price: 2 });
+		expect(product.id).toBeDefined();
+		expect(product.name).toBe('Product B');
+		expect(product.price).toBe(4);
+		expect(product.constructor.name).toBe('ProductB');
+	});
+
 });
