0ed46ab29e12e617c3381e466100aab120d00327
Isaias Mendes <isaiaszmendes@gmail.com>
2023-02-28 00:48:09 -0300

    test: create a customer with address using factory

--- src/domain/customer/factory/customer.factory.spec.ts
+++ src/domain/customer/factory/customer.factory.spec.ts
@@ -1,3 +1,4 @@
+import { Address } from '../value-object/Address';
 import { CustomerFactory } from './customer.factory';
 
 describe('Customer factory unit test', () => {
@@ -6,5 +7,14 @@ describe('Customer factory unit test', () => {
 		expect(customer.id).toBeDefined();
 		expect(customer.name).toBe('Ana');
 		expect(customer.address).toBeUndefined();
+	});
+
+	it('Should create a customer with an address', () => {
+		const address = new Address({ street: '15 de Novembro street', number: 25, city: 'são paulo', zip: '08047-000' });
+		const customer = CustomerFactory.createWithAddress({ name: 'Julia', address });
+
+		expect(customer.id).toBeDefined();
+		expect(customer.name).toBe('Julia');
+		expect(customer.address).toBe(address);
 	});
 });
