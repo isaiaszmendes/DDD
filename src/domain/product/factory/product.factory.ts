1b3aab464f7359e8e5307f5f52e79db7055f38a6
Isaias Mendes <isaiaszmendes@gmail.com>
2023-02-28 00:09:32 -0300

    feat: create product from a type

--- src/domain/product/factory/product.factory.ts
+++ src/domain/product/factory/product.factory.ts
@@ -1,5 +1,6 @@
 import { Product } from '../entity/product';
 import { randomUUID } from 'crypto';
+import { ProductB } from '../entity/product-b';
 
 interface ProductFactoryProps {
   type: string;
@@ -8,7 +9,14 @@ interface ProductFactoryProps {
 }
 
 export class ProductFactory {
-	public static create({ name, price }: ProductFactoryProps) {
-		return new Product({ id: randomUUID(), name, price });
+	public static create({ type, name, price }: ProductFactoryProps) {
+		switch (type) {
+		case 'a':
+			return new Product({ id: randomUUID(), name, price });
+		case 'b':
+			return new ProductB({ id: randomUUID(), name, price });
+		default:
+			throw new Error('Product type is not supported');
+		}
 	}
 }
